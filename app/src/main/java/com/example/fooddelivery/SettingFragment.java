eebd6761a34b5bb0636272855754ce74f6e21a0e
DolaKrishnan08 <dolakrishnan.seshachalam@softsuave.com>
2022-08-17 22:30:04 +0530

    Setting Screen Changes

--- app/src/main/java/com/example/fooddelivery/SettingFragment.java
+++ app/src/main/java/com/example/fooddelivery/SettingFragment.java
@@ -2,43 +2,59 @@ package com.example.fooddelivery;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fooddelivery.db.splash.SplashImage;
 import com.example.fooddelivery.db.splash.SplashImageRepository;
 import com.example.fooddelivery.helper.ImageConvertor;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class SettingFragment extends Fragment {
 
 
     ImageView customIV;
-    Button camera, save;
+    TextView camera, save;
 
     int MY_CAMERA_PERMISSION_CODE = 99;
     int CAMERA_REQUEST = 102;
+    int REQUEST_GET_SINGLE_FILE = 103;
 
     Bitmap bitmap;
 
     SplashImageRepository splashImageRepository;
+
+    RecyclerView recyclerView;
+    ImageAdapter imageAdapter;
 
     @Nullable
     @Override
@@ -49,32 +65,91 @@ public class SettingFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        customIV = view.findViewById(R.id.customImage);
-        camera = view.findViewById(R.id.camera);
-        save = view.findViewById(R.id.save);
+        camera = view.findViewById(R.id.addImage);
+        recyclerView = view.findViewById(R.id.rv);
 
         splashImageRepository = new SplashImageRepository(getActivity());
 
         camera.setOnClickListener(view1 -> {
 
-            openCamera();
+            showImageCaptureOption();
 
         });
-
-        save.setOnClickListener(view12 -> {
-            if (bitmap != null) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
-                String currentDateandTime = sdf.format(new Date());
-                byte[] bytes = ImageConvertor.convertBitmapToByteArray(bitmap);
-                Log.d("BytesData", Arrays.toString(bytes));
-                SplashImage splashImage = new SplashImage(currentDateandTime, bytes);
-                splashImageRepository.addImageData(splashImage);
-                Toast.makeText(getContext(), "Data Saved", Toast.LENGTH_SHORT).show();
-            }
-        });
+        List<byte[]> images = splashImageRepository.getAllImageData();
+        List<Bitmap> bitmaps_ = new ArrayList<>();
+        for (int i = 0; i < images.size(); i++) {
+            bitmaps_.add(ImageConvertor.convertByteArrayToBitmap(images.get(i)));
+        }
+        imageAdapter = new ImageAdapter(bitmaps_);
+        recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 3));
+        recyclerView.setAdapter(imageAdapter);
 
     }
 
+    void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_GET_SINGLE_FILE);
+    }
+
+    void saveImage() {
+        if (bitmap != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+            String currentDateandTime = sdf.format(new Date());
+            byte[] bytes = ImageConvertor.convertBitmapToByteArray(bitmap);
+            Log.d("BytesData", Arrays.toString(bytes));
+            SplashImage splashImage = new SplashImage(currentDateandTime, bytes);
+            splashImageRepository.addImageData(splashImage);
+            Toast.makeText(getContext(), "Please Wait....", Toast.LENGTH_SHORT).show();
+            if (imageAdapter != null) {
+                new Handler().postDelayed((Runnable) () -> {
+                    List<byte[]> images = splashImageRepository.getAllImageData();
+                    List<Bitmap> bitmaps = new ArrayList<>();
+                    for (int i = 0; i < images.size(); i++) {
+                        bitmaps.add(ImageConvertor.convertByteArrayToBitmap(images.get(i)));
+                    }
+                    imageAdapter.setImageBitmap(bitmaps);
+                    Toast.makeText(getContext(), "Data Saved", Toast.LENGTH_SHORT).show();
+                },500);
+            }
+        }
+    }
+
+
+    void showImageCaptureOption() {
+        // Dialogue with Multiple URL Instance
+        View view = LayoutInflater.from(requireContext()).inflate(R.layout.choose_camera, null, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog);
+        builder.setView(view);
+        Dialog dialog = builder.create();
+
+        TextView camera = view.findViewById(R.id.camera);
+        TextView gallery = view.findViewById(R.id.gallery);
+
+        //On Click Listener
+        camera.setOnClickListener(v1 -> {
+            openCamera();
+            dialog.dismiss();
+        });
+        gallery.setOnClickListener(v2 -> {
+            openGallery();
+            dialog.dismiss();
+        });
+
+
+        dialog.setCancelable(false);
+        dialog.show();
+        // Make The Dialogue width 80% of Full Screen
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int displayWidth = displayMetrics.widthPixels;
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+        layoutParams.width = (int) (displayWidth * 0.8f);
+        dialog.getWindow().setAttributes(layoutParams);
+    }
 
     void openCamera() {
 
@@ -99,6 +174,12 @@ public class SettingFragment extends Fragment {
             } else {
                 Toast.makeText(getContext(), "camera permission denied", Toast.LENGTH_LONG).show();
             }
+        } else if (requestCode == REQUEST_GET_SINGLE_FILE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(getContext(), "camera permission denied", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -107,7 +188,16 @@ public class SettingFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             bitmap = (Bitmap) data.getExtras().get("data");
-            customIV.setImageBitmap(bitmap);
+            saveImage();
+        } else if (requestCode == REQUEST_GET_SINGLE_FILE && resultCode == Activity.RESULT_OK) {
+            try {
+                Uri imageUri = data.getData();
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), imageUri);
+                saveImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
--- app/src/main/java/com/example/fooddelivery/helper/ImageConvertor.java
+++ app/src/main/java/com/example/fooddelivery/helper/ImageConvertor.java
@@ -8,10 +8,27 @@ import java.io.ByteArrayOutputStream;
 public class ImageConvertor {
 
     public static byte[] convertBitmapToByteArray(Bitmap bitmap) {
+        bitmap = getResizedBitmap(bitmap,500);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
         return byteArrayOutputStream.toByteArray();
     }
+
+    public static Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
 
     public static Bitmap convertByteArrayToBitmap(byte[] imageBytes) {
         return BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.length);
